15afbb017a620bec59838810b7d4bce1b1c7928e
jbum-digisynd <jbum@jbum.com>
2016-03-15 13:28:25 -0700

    Updated Perl script to include the version with scales.

--- perl/makeWhitneyMusic.pl
+++ perl/makeWhitneyMusic.pl
@@ -1,30 +1,15 @@
 #!/usr/bin/perl
 
 #
-# makeWhitneyMusic.pl - Jim Bumgardner
+# makeWhitneyMusic - Jim Bumgardner
 #
-# This Perl script will produce standard MIDI files containing Whitney Music Box sequences.
-#
-# NOTE: Most of the variations on the Whitney Music Box were not
-# made using MIDI. Some of them, such as the ones based on harmonics
-# or on microtones, cannot be made using a MIDI instrument because of
-# the precise tunings involved. Instead, I generated the audio directly
-# using my software synthesizer "Syd".  PD (Puredata) is also a good choice.
-#
-# I did use MIDI for some of the later variations which use Piano sounds 
-# (particularly #12 and #13.  For #11, which uses a different piano sound,
-# I used a more elegant Nyquist script.
-#
-# This is the script I used for #12.
-#
-# - Jim Bumgardner
 
 # use Data::Dumper;  # for debugging
 use MIDI;
 
 $syntax = <<EOT;
 
-makeWhitneyMidi [options]
+makeWhitneyMidi [options - please include at least one]
 
 -o outfile           name of output file (default=o.mid)
 -tines n             nbr tines
@@ -33,6 +18,8 @@ makeWhitneyMidi [options]
 -rev                 reverse pitches
 -rise                rising pitches (k*n % tines)
 -primes              prime numbers
+-nonprimes           non primes
+-scale scale         choice of chromatic blues blues2 major minor minorh (default=chromatic)
 EOT
 
 $ofile = 'o.mid';
@@ -49,9 +36,19 @@ $maxAmp = 300;
 $rev = 0;
 $xor = 0;
 $rise = 0;
-$primes = 0;
+$usePrimes = 0;
+$useNonPrimes = 0;
+$useScale = 0;
 
-@ptab = (   2,   3,   5,   7,  11,  13,  17,  19,  23,  29,
+%scales = (blues => [0,3,4,5,6,7,10],
+           blues2 => [0,2,3,4,5,6,7,10],
+           major => [0,2,4,5,7,9,11],
+           lydian => [0,2,4,6,7,9,11],
+           minor => [0,2,3,5,7,8,10],
+           minorh => [0,2,3,5,7,8,11]);
+
+
+@primes = (   2,   3,   5,   7,  11,  13,  17,  19,  23,  29,
            31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
            73,  79,  83,  89,  97, 101, 103, 107, 109, 113, 
           127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
@@ -59,6 +56,21 @@ $primes = 0;
           233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
           283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
           353, 359, 367, 373, 379, 383);
+
+@nonprimes = (   1,   4,   6,   8,   9,  10,  12,  14,  15,  16,
+           18,  20,  21,  22,  24,  25,  26,  27,  28,  30,
+           32,  33,  34,  35,  36,  38,  39,  40,  42,  44, 
+           45,  46,  48,  49,  50,  51,  52,  54,  55,  56,
+           57,  58,  60,  62,  63,  64,  65,  66,  68,  69, 
+           70,  72,  74,  75,  76,  77,  78,  80,  81,  82,
+           84,  85,  86,  87,  88,  90,  91,  92,  93,  94,
+           95,  96,  98,  99, 100, 102);
+
+if (scalar(@ARGV) == 0)
+{
+  print $syntax;
+  exit;
+}
 
 while ($_ = shift)
 {
@@ -88,26 +100,47 @@ while ($_ = shift)
   }
   elsif (/^-primes?$/)
   {
-    $primes = 1;
+    $usePrimes = 1;
+  }
+  elsif (/^-scale$/)
+  {
+    $useScale = lc(shift);
+    undef $useScale if $useScale eq 'chromatic';
+    die ("Unknown scale: $useScale\n") if !(defined $scales{$useScale});
+  }
+  elsif (/^-nonprimes?$/)
+  {
+    $useNonPrimes = 1;
   }
   elsif (/^-o$/)
   {
     $ofile = shift;
   }
   else {
+    print "Unknown: $_" . "\n\n";
     print $syntax;
     exit;
   }
 }
 
-sub kToPitch($$)
+
+my $fPitch = int(64-$nbrTines/2);
+my $lenScale = 12;
+if ($useScale) {
+    $lenScale = scalar(@{$scales{$useScale}});
+    $fPitch = int(64-($nbrTines*12/$lenScale)/2);
+}
+
+sub kToPitch($$)  # n has no effect if rise is not in effect
 {
   my ($k,$n) = @_;
-  
-  my $fPitch = int(64-$nbrTines/2);
-  $fPitch = 21 if $fPitch < 21;
 
-  $k = $k ^ 0x02A if ($xor);
+  if ($useScale) {
+    $k = $scales{$useScale}->[$k % $lenScale] + 12*int($k/$lenScale);
+  }
+  else {
+    $k = $k ^ 0x02A if ($xor);
+  }
   if ($rev)
   {
      return ($fPitch+$nbrTines) - $k;
@@ -146,7 +179,8 @@ sub kToVelocity($$)
 sub kToCount($)
 {
   my ($k) = @_;
-  return $ptab[$k] if $primes;
+  return $primes[$k] if $usePrimes;
+  return $nonprimes[$k] if $useNonPrimes;
   return ($k+1);
 }
 
@@ -202,4 +236,4 @@ MIDI::Track->new({
 
 ]
 });
-$op->write_to_file("$ofile");
+$op->write_to_file("$ofile");  
